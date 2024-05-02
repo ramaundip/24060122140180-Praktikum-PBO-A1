018cbd5398f8dafeffd2b19e07fa5c75403e085d
Muhammad Anugerah Ramadhan <142155868+ramaundip@users.noreply.github.com>
2024-05-02 08:08:27 +0700

    Add files via upload

---
+++ Praktikum 7/Senjata.java
@@ -0,0 +1,35 @@
+public class Senjata {
+    private String bunyi;
+    private int peluru;
+
+    public Senjata(String bunyi) {
+        this.bunyi = bunyi;
+        this.peluru = 0;
+    }
+
+    public String getBunyi() {
+        return bunyi;
+    }
+
+    public int getPeluru() {
+        return peluru;
+    }
+
+    public void menembak() {
+        if (peluru > 0) {
+            System.out.println(bunyi);
+            peluru--;
+            System.out.println("Sisa Peluru: " +peluru);
+        } else {
+            System.out.println("Tidak ada peluru!");
+        }
+    }
+
+    public void setBunyi(String bunyi) {
+        this.bunyi = bunyi;
+    }
+
+    public void setPeluru(int peluru) {
+        this.peluru = peluru;
+    }
+}
