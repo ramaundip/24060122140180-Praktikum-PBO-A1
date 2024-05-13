398032ac93f9e385aadaefdbe929510606c0d9f1
Muhammad Anugerah Ramadhan <142155868+ramaundip@users.noreply.github.com>
2024-05-13 21:54:22 +0700

    Delete Praktikum 8 directory

--- Praktikum 8/kupu.java
+++
@@ -1,3 +0,0 @@
-public abstract class kupu{
-    public abstract void gerak();
-}
--- Praktikum 8/ulat.java
+++
@@ -1,6 +0,0 @@
-class ulat extends kupu {
-    public void gerak() {
-      System.out.println("ulat merayap");
-    }
-  }
-  
